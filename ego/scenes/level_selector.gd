0332e5bf57ad73f5a44171f72fa528a0a033a109
BalintXHD <balint.tablet@gmail.com>
2024-12-05 11:15:10 +0100

    asdfgh

--- ego/scenes/level_selector.gd
+++ ego/scenes/level_selector.gd
@@ -9,7 +9,10 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	EgoVenture.save_continue()
-	if(Boombox.get_music().get_length() != preload("res://music/main_menu.mp3").get_length()):
+	if(Boombox.is_music_playing()):
+		if(Boombox.get_music().get_length() != preload("res://music/main_menu.mp3").get_length()):
+			Boombox.play_music(preload("res://music/main_menu.mp3"))
+	else:
 		Boombox.play_music(preload("res://music/main_menu.mp3"))
 	if(!(EgoVenture.state as GameState).easy_done):
 		get_node("TextureButton3").modulate = Color(0.5,0.5,0.5,1)
@@ -37,6 +40,7 @@ func _on_Button2_pressed():
 	(EgoVenture.state as GameState).easy_has_coded_message = false
 	(EgoVenture.state as GameState).easy_has_number_code = false
 
+
 func _on_Button3_pressed():
 	if(get_node("TextureButton3").modulate == Color(1,1,1,1)):
 		#EgoVenture.change_scene("res://scenes/level_medium/level_medium.tscn")
@@ -58,3 +62,19 @@ func _on_Button3_pressed():
 
 func _on_TextureButton4_pressed():
 	MainMenu.toggle()
+
+
+
+func _on_MapHotspot_activate():
+	(EgoVenture.state as GameState).medium_can_gyufa = false
+	(EgoVenture.state as GameState).medium_can_labtorlo = false
+	(EgoVenture.state as GameState).medium_done = false
+	(EgoVenture.state as GameState).medium_door_unlocked = false
+	(EgoVenture.state as GameState).medium_has_key = false
+	(EgoVenture.state as GameState).medium_has_mainmessage1 = false
+	(EgoVenture.state as GameState).medium_has_mainmessage2 = false
+	(EgoVenture.state as GameState).medium_has_natobook = false
+	(EgoVenture.state as GameState).medium_has_natomessage = false
+	(EgoVenture.state as GameState).medium_has_periodic = false
+	(EgoVenture.state as GameState).medium_has_research = false
+
