50400572c8b5fb44e579f13c1c3f1206cb0ad628
BalintXHD <balint.tablet@gmail.com>
2024-12-05 11:43:45 +0100

    akurvafinal

--- ego/scenes/level_medium/success.gd
+++ ego/scenes/level_medium/success.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Parrot.connect("dialog_finished", self, "_on_dialog_finished")
+	Parrot.connect("finished_dialog", self, "_on_dialog_finished")
 	Boombox.play_music(preload("res://music/victory.mp3"))
 	Inventory.remove_item(preload("res://inventory/level_medium_key.tres"))
 	Inventory.disable()
@@ -19,7 +19,7 @@ func _ready():
 
 
 # Define the signal handler
-func _on_dialog_finished():
+func _on_dialog_finished(sajt):
 	get_node("Hotspot").disabled = false
 	Parrot.skip_enabled = true
 	(EgoVenture.state as GameState).medium_done = true
